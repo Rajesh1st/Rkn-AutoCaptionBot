a68bb085799ef352985badc704ef22f32efeb0aa
Rajesh1st <rajeshtb55@outlook.com>
2025-01-10 19:52:51 +0530

    Update database.py

--- Rkn_Bots/database.py
+++ Rkn_Bots/database.py
@@ -20,49 +20,51 @@ async def insert(user_id):
     except:
         pass
 
-# Total User
+# Total users
 async def total_user():
     user = await users.count_documents({})
     return user
 
-# Total Channels
+# Total channels
 async def total_channels():
     channels = await chnl_ids.count_documents({})
     return channels
 
-# Get all users
+# Get all user IDs
 async def getid():
     all_users = users.find({})
     return all_users
 
-# Delete user
+# Delete a user by ID
 async def delete(id):
-    await users.delete_one(id)
-                     
-# Add caption
+    await users.delete_one({"_id": id})
+
+# Add caption for a channel
 async def addCap(chnl_id, caption):
     dets = {"chnl_id": chnl_id, "caption": caption}
     await chnl_ids.insert_one(dets)
 
-# Update caption
+# Update caption for a channel
 async def updateCap(chnl_id, caption):
-    await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"caption": caption}})
+    await chnl_ids.update_one(
+        {"chnl_id": chnl_id}, 
+        {"$set": {"caption": caption}}
+    )
 
 # Increment media edit count for a channel
 async def increment_media_edit_count(chnl_id):
     await chnl_ids.update_one(
         {"chnl_id": chnl_id},
-        {"$inc": {"edited_count": 1}},  # Increment by 1 each time a message is edited
-        upsert=True  # If channel doesn't exist, it will be created
+        {"$inc": {"edited_count": 1}},  # Increment `edited_count` by 1
+        upsert=True  # Create document if it doesn't exist
     )
 
-# Function to get total number of edits for all channels
+# Get total number of media edits for all channels
 async def get_total_media_edits():
     total_edits = await chnl_ids.aggregate([
         {"$group": {"_id": None, "total_edits": {"$sum": "$edited_count"}}}
-    ]).to_list(length=1)  # This will return the total edited message count
+    ]).to_list(length=1)
     return total_edits[0]["total_edits"] if total_edits else 0
-
 
 # Rkn Developer 
 # Don't Remove Credit 😔
