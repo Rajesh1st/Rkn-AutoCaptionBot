e890f6601819a6be98712089271246c6c772d5c8
Rajesh1st <rajeshtb55@outlook.com>
2025-01-10 20:37:14 +0530

    Update database.py

--- Rkn_Bots/database.py
+++ Rkn_Bots/database.py
@@ -12,14 +12,14 @@ db = client[Rkn_Bots.DB_NAME]
 chnl_ids = db.chnl_ids
 users = db.users
 
-# Insert user data
+#insert user data
 async def insert(user_id):
     user_det = {"_id": user_id}
     try:
         await users.insert_one(user_det)
     except:
         pass
-
+        
 # Total User
 async def total_user():
     user = await users.count_documents({})
@@ -30,46 +30,19 @@ async def total_channels():
     channels = await chnl_ids.count_documents({})
     return channels
 
-# Get all user IDs
 async def getid():
     all_users = users.find({})
     return all_users
 
-# Delete a user by ID
 async def delete(id):
-    await users.delete_one({"_id": id})
-
-# Add caption for a channel
+    await users.delete_one(id)
+                     
 async def addCap(chnl_id, caption):
-    dets = {"chnl_id": chnl_id, "caption": caption, "edited_count": 0}  # Initialize edit count
+    dets = {"chnl_id": chnl_id, "caption": caption}
     await chnl_ids.insert_one(dets)
 
-# Update caption for a channel
 async def updateCap(chnl_id, caption):
-    await chnl_ids.update_one(
-        {"chnl_id": chnl_id},
-        {"$set": {"caption": caption}}
-    )
-
-# Increment media edit count for a channel
-async def increment_media_edit_count(chnl_id):
-    await chnl_ids.update_one(
-        {"chnl_id": chnl_id},
-        {"$inc": {"edited_count": 1}},  # Increment `edited_count` by 1
-        upsert=True
-    )
-
-# Get the caption for a specific channel
-async def get_caption(chnl_id):
-    data = await chnl_ids.find_one({"chnl_id": chnl_id})
-    return data["caption"] if data else None
-
-# Get total number of media edits for all channels
-async def get_total_media_edits():
-    total_edits = await chnl_ids.aggregate([
-        {"$group": {"_id": None, "total_edits": {"$sum": "$edited_count"}}}
-    ]).to_list(length=1)
-    return total_edits[0]["total_edits"] if total_edits else 0
+    await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"caption": caption}})
 
 # Rkn Developer 
 # Don't Remove Credit 😔
