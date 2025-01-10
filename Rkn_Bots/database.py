a7221a09b67e56a6aa69c117f84d854ea96f8cc9
Rajesh1st <rajeshtb55@outlook.com>
2025-01-10 13:05:02 +0530

    Update database.py

--- Rkn_Bots/database.py
+++ Rkn_Bots/database.py
@@ -11,15 +11,16 @@ client = motor.motor_asyncio.AsyncIOMotorClient(Rkn_Bots.DB_URL)
 db = client[Rkn_Bots.DB_NAME]
 chnl_ids = db.chnl_ids
 users = db.users
+media_edits = db.media_edits  # New collection for media edit tracking
 
-#insert user data
+# Insert user data
 async def insert(user_id):
     user_det = {"_id": user_id}
     try:
         await users.insert_one(user_det)
     except:
         pass
-        
+
 # Total User
 async def total_user():
     user = await users.count_documents({})
@@ -29,6 +30,25 @@ async def total_user():
 async def total_channels():
     channels = await chnl_ids.count_documents({})
     return channels
+
+# Increment media edit count for a channel
+async def increment_media_edit_count(channel_id):
+    result = await media_edits.update_one(
+        {'channel_id': channel_id},  # Find the channel by ID
+        {'$inc': {'edit_count': 1}},  # Increment the media edit count
+        upsert=True  # If the channel doesn't exist, create a new one with edit_count = 1
+    )
+    return result.modified_count > 0  # Return True if update was successful
+
+# Get total media edits across all channels
+async def get_total_media_edits():
+    total_edits = await media_edits.aggregate([
+        {"$group": {"_id": None, "total": {"$sum": "$edit_count"}}}
+    ]).to_list(length=1)
+    
+    if total_edits:
+        return total_edits[0]['total']
+    return 0  # Return 0 if no media edits are found
 
 async def getid():
     all_users = users.find({})
