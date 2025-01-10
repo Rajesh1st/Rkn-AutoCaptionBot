87e47949e788425fc182c71fde10d2dbe40fd22e
Rajesh1st <rajeshtb55@outlook.com>
2025-01-10 16:59:41 +0530

    Update database.py

--- Rkn_Bots/database.py
+++ Rkn_Bots/database.py
@@ -12,7 +12,7 @@ db = client[Rkn_Bots.DB_NAME]
 chnl_ids = db.chnl_ids
 users = db.users
 
-# insert user data
+# Insert user data
 async def insert(user_id):
     user_det = {"_id": user_id}
     try:
@@ -30,22 +30,26 @@ async def total_channels():
     channels = await chnl_ids.count_documents({})
     return channels
 
+# Get all users
 async def getid():
     all_users = users.find({})
     return all_users
 
+# Delete user
 async def delete(id):
     await users.delete_one(id)
                      
+# Add caption
 async def addCap(chnl_id, caption):
     dets = {"chnl_id": chnl_id, "caption": caption}
     await chnl_ids.insert_one(dets)
 
+# Update caption
 async def updateCap(chnl_id, caption):
     await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"caption": caption}})
 
-# Add this new function to count edited messages globally
-async def increment_edit_count(chnl_id):
+# Increment media edit count for a channel
+async def increment_media_edit_count(chnl_id):
     await chnl_ids.update_one(
         {"chnl_id": chnl_id},
         {"$inc": {"edited_count": 1}},  # Increment by 1 each time a message is edited
@@ -53,7 +57,7 @@ async def increment_edit_count(chnl_id):
     )
 
 # Function to get total number of edits for all channels
-async def total_edits():
+async def get_total_media_edits():
     total_edits = await chnl_ids.aggregate([
         {"$group": {"_id": None, "total_edits": {"$sum": "$edited_count"}}}
     ]).to_list(length=1)  # This will return the total edited message count
