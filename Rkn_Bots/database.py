7077a6292a0b1e4c01071827c3674ec64fa93cac
Rajesh1st <rajeshtb55@outlook.com>
2025-01-12 01:08:57 +0530

    Update database.py

--- Rkn_Bots/database.py
+++ Rkn_Bots/database.py
@@ -7,46 +7,42 @@
 import motor.motor_asyncio
 from config import Rkn_Bots
 
-# Database connection setup
 client = motor.motor_asyncio.AsyncIOMotorClient(Rkn_Bots.DB_URL)
 db = client[Rkn_Bots.DB_NAME]
 chnl_ids = db.chnl_ids
 users = db.users
 
-# Insert user data
+#insert user data
 async def insert(user_id):
     user_det = {"_id": user_id}
     try:
         await users.insert_one(user_det)
     except:
         pass
-
-# Total user count
+        
+# Total User
 async def total_user():
     user = await users.count_documents({})
     return user
 
-# Total channel count
+# Total Channels
 async def total_channels():
     channels = await chnl_ids.count_documents({})
     return channels
 
-# Get all users
 async def getid():
     all_users = users.find({})
     return all_users
 
-# Delete user
 async def delete(id):
     await users.delete_one(id)
-
-# Add or update button for a specific channel
-async def addCap(chnl_id, button_data):
-    dets = {"chnl_id": chnl_id, **button_data}
+                     
+async def addCap(chnl_id, caption):
+    dets = {"chnl_id": chnl_id, "caption": caption}
     await chnl_ids.insert_one(dets)
 
-async def updateCap(chnl_id, button_data):
-    await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": button_data})
+async def updateCap(chnl_id, caption):
+    await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"caption": caption}})
 
 # Rkn Developer 
 # Don't Remove Credit 😔
