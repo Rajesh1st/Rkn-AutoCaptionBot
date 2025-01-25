8fe4f5971ecf01cfcbea262906743b770111db23
Rajesh1st <rajeshtb55@outlook.com>
2025-01-25 21:08:21 +0530

    Update database.py

--- Rkn_Bots/database.py
+++ Rkn_Bots/database.py
@@ -11,16 +11,17 @@ client = motor.motor_asyncio.AsyncIOMotorClient(Rkn_Bots.DB_URL)
 db = client[Rkn_Bots.DB_NAME]
 chnl_ids = db.chnl_ids
 users = db.users
+banned_users = db.banned_users  # New collection for banned users
 
-#insert user data
+# Insert user data
 async def insert(user_id):
     user_det = {"_id": user_id}
     try:
         await users.insert_one(user_det)
     except:
         pass
-        
-# Total User
+
+# Total Users
 async def total_user():
     user = await users.count_documents({})
     return user
@@ -36,7 +37,7 @@ async def getid():
 
 async def delete(id):
     await users.delete_one(id)
-                     
+
 async def addCap(chnl_id, caption):
     dets = {"chnl_id": chnl_id, "caption": caption}
     await chnl_ids.insert_one(dets)
@@ -44,7 +45,19 @@ async def addCap(chnl_id, caption):
 async def updateCap(chnl_id, caption):
     await chnl_ids.update_one({"chnl_id": chnl_id}, {"$set": {"caption": caption}})
 
-# Rkn Developer 
-# Don't Remove Credit 😔
-# Telegram Channel @RknDeveloper & @Rkn_Bots
-# Developer @RknDeveloperr
+# Ban a user
+async def ban_user(user_id):
+    user = {"_id": user_id}
+    try:
+        await banned_users.insert_one(user)
+    except:
+        pass
+
+# Unban a user
+async def unban_user(user_id):
+    await banned_users.delete_one({"_id": user_id})
+
+# Check if a user is banned
+async def is_banned(user_id):
+    user = await banned_users.find_one({"_id": user_id})
+    return user is not None
